ca7f3bfe84403bc2d2b29acaedd6255dc417e810
erick <erick@DEKU-PC>
2023-02-28 10:41:54 -0300

    Primeira Atualização

--- src/controller/RedesController.java
+++ src/controller/RedesController.java
@@ -10,19 +10,21 @@ public class RedesController {
 		super();
 	}
 	//Retornar o Sistema Operacional da Máquina
-	public String os() {
+	private String os() {
 		String os = System.getProperty("os.name");
 		return os;	
 	}
     //Mostra as Configurações IP do SO
 	public void ip(String process) {
-		try {
-			Process p = Runtime.getRuntime().exec(process);
-			InputStream fluxo = p.getInputStream();
-			InputStreamReader leitor = new InputStreamReader(fluxo);
-			BufferedReader buffer = new BufferedReader(leitor);
-			String linha = buffer.readLine();
-			while(linha != null) {
+		os();
+		if(os().contains("Windows")) {
+		     try {
+			 Process p = Runtime.getRuntime().exec(process);
+			 InputStream fluxo = p.getInputStream();
+			 InputStreamReader leitor = new InputStreamReader(fluxo);
+			 BufferedReader buffer = new BufferedReader(leitor);
+			 String linha = buffer.readLine();
+			 while(linha != null) {
 				linha = buffer.readLine();
 				if(linha == null) {
 					linha = buffer.readLine();
@@ -38,10 +40,38 @@ public class RedesController {
 			String msgerro = e.getMessage();
 			System.err.println(msgerro);
 		}
+		     }else {
+			String processIP = "IFCONFIG";
+			try {
+				 Process p = Runtime.getRuntime().exec(process);
+				 InputStream fluxo = p.getInputStream();
+				 InputStreamReader leitor = new InputStreamReader(fluxo);
+				 BufferedReader buffer = new BufferedReader(leitor);
+				 String linha = buffer.readLine();
+				 while(linha != null) {
+					linha = buffer.readLine();
+					if(linha == null) {
+						linha = buffer.readLine();
+					}else if(linha.contains("Wi-Fi") || linha.contains("IPv4") || linha.contains("Ethernet Ethernet ") ) {
+						String[] adap = linha.split(" ", 1);
+						System.out.println(adap[0]);
+				   }
+				}
+				buffer.close();
+				leitor.close();
+				fluxo.close();
+			}catch(IOException e) {
+				String msgerro = e.getMessage();
+				System.err.println(msgerro);
+			}
+			
+		}
 	}
 	//Mostra tempo Médio de resposta com a URL www.google.com.br do SO
 	public void ping(String process){
+		os();
 		System.out.println("Aguarde...");
+		if(os().contains("Windows")) {
 		try {
 			Process p = Runtime.getRuntime().exec(process);
 			InputStream fluxo = p.getInputStream();
@@ -65,8 +95,35 @@ public class RedesController {
 			String msgerro = e.getMessage();
 			System.err.println(msgerro);
 		}
+      }else {
+    	  String processPing = "PING -4 -c 10 www.google.com.br"; 
+    	  try {
+  			Process p = Runtime.getRuntime().exec(process);
+  			InputStream fluxo = p.getInputStream();
+  			InputStreamReader leitor = new InputStreamReader(fluxo);
+  			BufferedReader buffer = new BufferedReader(leitor);
+  			String linha = buffer.readLine();
+  			while(linha != null) {
+  				linha = buffer.readLine();
+  				if(linha == null) {
+  					linha = buffer.readLine();
+  				}
+  				else if(linha.contains("dia = ")) {
+  					String[] Media = linha.split(", ", 4);
+  			     	System.out.println("Resultado  ==> "+Media[2]);
+  			 }
+  			}
+  			buffer.close();
+  			leitor.close();
+  			fluxo.close();
+  		}catch(IOException e) {
+  			String msgerro = e.getMessage();
+  			System.err.println(msgerro);
+  		}
+        }
       }
-}
+
+	}
 
 
 
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -10,34 +10,17 @@ public class Principal {
 		RedesController redcon = new RedesController();
 		int opc = 0;
 		while(opc != 9) {
-			opc = Integer.parseInt(JOptionPane.showInputDialog("Escolha:\n1 - Mostrar Nome da Máquina\n2 - Mostrar o IP\n3 - Mostrar Ping\n9 - FIM "));
+			opc = Integer.parseInt(JOptionPane.showInputDialog("Escolha:\n1 - Mostrar o IP\n2 - Mostrar Ping\n9 - FIM "));
 			switch(opc) {
 			//Método 1
 			case 1:
-			 String os = redcon.os();
-			 System.out.println(os);
-			break;
-			//Método 2
-			case 2:
-			 redcon.os();
-			 if(redcon.os().contains("Windows")) {
 			 String processIP = "IPCONFIG";
 			 redcon.ip(processIP);
-			 }else{
-				 String processIP = "IFCONFIG";
-				 redcon.ip(processIP);
-			 }
 			 break;
-			//Método 3
-			case 3:
-			 redcon.os();
-			 if(redcon.os().contains("Windows")) {
+			//Método 2
+			case 2: 
 			 String processPing = "PING -4 -n 10 www.google.com.br";
 			 redcon.ping(processPing);
-			 }else {
-				 String processPing = "PING -4 -c 10 www.google.com.br";
-				 redcon.ping(processPing);
-			 }
 			 break;
 			case 9:JOptionPane.showMessageDialog(null, "FIM");
 			break;
